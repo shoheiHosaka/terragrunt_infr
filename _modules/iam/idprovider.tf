c0a0bd348ec1d6d756ac57f08b05992dfc801a07
Shohei Hosaka <shohei.hosaka@gmail.com>
2023-03-05 02:01:42 +0900

    もっかいテスト

--- _modules/iam/idprovider.tf
+++ _modules/iam/idprovider.tf
@@ -32,7 +32,7 @@ data "aws_iam_policy_document" "github_assume_policy" {
     condition {
       test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:aud"
-      values   = ["sts.amazonaws.com"]
+      values   = ["sts.amazonaws.com" ]
     }
   }
 }
